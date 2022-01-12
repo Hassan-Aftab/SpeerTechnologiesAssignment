92c8484b5638d861cba915089eb2d8ce13d272e5
Hassan Aftab <Hassan.aftab@agoda.com>
2022-01-12 21:51:42 +0500

    Unit test added for search screen

--- SpeerTechnologiesAssignmentTests/SpeerTechnologiesAssignmentTests.swift
+++ SpeerTechnologiesAssignmentTests/SpeerTechnologiesAssignmentTests.swift
@@ -8,26 +8,151 @@
 import XCTest
 @testable import SpeerTechnologiesAssignment
 
-class SpeerTechnologiesAssignmentTests: XCTestCase {
+class SearchUserViewModelTests: XCTestCase {
 
+    var mockService: SearchUserHandler!
+    var viewModel: SearchUserViewModel!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockService = MockSearchUserAPI()
+        viewModel = SearchUserViewModel(mockService)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockService = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSearchBarBeginEditing() throws {
+        var events = 0
+        viewModel.output.showSearchBarCancelButton = { _ in
+            events += 1
         }
+        viewModel.input.searchBarBeginEditing?()
+        XCTAssertEqual(events, 1)
     }
 
+    func testSearchBarEndEditing() throws {
+        var events = 0
+        viewModel.output.showSearchBarCancelButton = { _ in
+            events += 1
+        }
+
+        viewModel.input.searchBarEndEditing?()
+        XCTAssertEqual(events, 1)
+    }
+
+    func testSearchBarCancel() throws {
+        var events = 0
+
+        viewModel.output.showSearchBarCancelButton = { _ in
+            events += 1
+        }
+
+        viewModel.output.clearSearchBar = {
+            events += 1
+        }
+
+        viewModel.output.resignSearchBar = {
+            events += 1
+        }
+
+        viewModel.input.searchBarCancel?()
+
+        XCTAssertEqual(events, 3)
+    }
+
+    func testSearchSuccess() {
+        var events = 0
+        var user : User?
+
+        viewModel.output.setLoaderHidden = { _ in
+            events += 1
+        }
+
+        viewModel.output.showSearchBarCancelButton = { _ in
+            events += 1
+        }
+
+        viewModel.output.showUser = { response in
+            user = response
+        }
+
+        viewModel.input.search?("Hassan-Aftab")
+
+        XCTAssertEqual(events, 3)
+        XCTAssertNotNil(user)
+        XCTAssertEqual(user?.username, "Hassan-Aftab")
+        XCTAssertEqual(user?.name, "Hassan Aftab")
+    }
+
+    func testSearchNoUserFound() {
+        var events = 0
+
+        viewModel.output.setLoaderHidden = { _ in // called twice
+            events += 1
+        }
+        viewModel.output.showSearchBarCancelButton = { _ in
+            events += 1
+        }
+        viewModel.output.userNotFound = {
+            events += 1
+        }
+
+        viewModel.input.search?("Hassan")
+
+        XCTAssertEqual(events, 4)
+    }
+
+    func testSearchError() {
+        var events = 0
+        var error: Error?
+
+        viewModel.output.setLoaderHidden = { _ in // called twice
+            events += 1
+        }
+        viewModel.output.showSearchBarCancelButton = { _ in
+            events += 1
+        }
+        viewModel.output.onError = { err in
+            events += 1
+            error = err
+        }
+
+        viewModel.input.search?("-")
+
+        XCTAssertEqual(events, 4)
+        XCTAssert(error is MockError)
+    }
+}
+
+class MockSearchUserAPI: SearchUserHandler {
+    func searchUser(username: String, completion: @escaping CompletionHandler<SearchUserResponse>) {
+        if username == "-" {
+            completion(.failure(MockError.invalidInput))
+            return
+        }
+        if username == "Hassan-Aftab" {
+
+            completion(.success(MockUserResponse.getMockUser()))
+            return
+        }
+        completion(.success(MockUserResponse.getMockNoUserFound()))
+    }
+}
+
+class MockUserResponse {
+    static func getMockUser() -> SearchUserResponse {
+        let url = URL(fileURLWithPath: Bundle(for: type(of: MockUserResponse())).path(forResource: "MockUser", ofType: "json")!)
+        let data = try! Data(contentsOf: url)
+        return try! JSONDecoder().decode(SearchUserResponse.self, from: data)
+    }
+
+    static func getMockNoUserFound() -> SearchUserResponse {
+        let url = URL(fileURLWithPath: Bundle(for: type(of: MockUserResponse())).path(forResource: "MockNoUserFound", ofType: "json")!)
+        let data = try! Data(contentsOf: url)
+        return try! JSONDecoder().decode(SearchUserResponse.self, from: data)
+    }
+}
+
+enum MockError: Error {
+    case invalidInput
 }
